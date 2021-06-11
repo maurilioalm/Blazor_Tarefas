4b15a3b7bc42795e72003cf464e8740b3b2bfe6f
Maurilio Medeiros <maurilioalm@hotmail.com>
2021-06-10 21:29:14 -0300

    Teste nova Branch

--- Blazor_Tarefas/wwwroot/JSFunctions.js
+++ Blazor_Tarefas/wwwroot/JSFunctions.js
@@ -1,3 +1,16 @@
 ﻿window.MostrarAlerta = (message) => {
     alert(message);
 }
+function getTotalTarefas() {
+    DotNet.invokeMethodAsync("Blazor_Tarefas", "ObterTotalTarefas")
+        .then(resultado => {
+            alert("Total de tarefas: " + resultado);
+        });
+}
+
+function getTotalTarefasInstancia(dotnet) {
+    return dotnet.invokeMethodAsync("ObterTotalTarefasInstancia")
+        .then(resultado => {
+            alert("Total de tarefas: " + resultado);
+        });
+}
